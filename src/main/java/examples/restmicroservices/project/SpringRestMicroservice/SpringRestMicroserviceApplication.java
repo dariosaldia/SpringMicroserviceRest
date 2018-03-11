a00b6889d6b879a3ce322876b7ea7be82c1c9810
Dario <dariosaldia@gmail.com>
2018-03-10 22:02:58 -0300

    Initial Project Set Up

---
+++ src/main/java/examples/restmicroservices/project/SpringRestMicroservice/SpringRestMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package examples.restmicroservices.project.SpringRestMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestMicroserviceApplication.class, args);
+	}
+}
